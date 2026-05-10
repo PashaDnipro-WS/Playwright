bbfb259cc659eccd197eb6e77c107f6031d4f3b3
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-05-10 21:21:54 +0300

    extend data of the valid test user

--- data/users.ts
+++ data/users.ts
@@ -1,23 +1,26 @@
 export const users = {
-  validUser: {
-    login: process.env.LOGIN!,
-    password: process.env.PASSWORD!,
-  },
+    validUser: {
+        login: process.env.LOGIN!,
+        password: process.env.PASSWORD!,
+        firstName: 'Lucky',
+        lastName: 'User',
+        email: 'luckyautomationuser+10@gmail.com',
+    },
 
-  invalidUser: {
-    login: 'wrong_user',
-    password: 'wrong_password',
-  },
+    invalidUser: {
+        login: 'wrong_user',
+        password: 'wrong_password',
+    },
 };
 
 export const generateRegisterUser = () => {
-  const random = Date.now();
+    const random = Date.now();
 
-  return {
-    login: `test_user_${random}`,
-    password: `Password_${random}`,
-    firstName: 'Test',
-    lastName: 'User',
-    email: `test_${random}@mail.com`,
-  };
+    return {
+        login: `test_user_${random}`,
+        password: `Password_${random}`,
+        firstName: 'Test',
+        lastName: 'User',
+        email: `test_${random}@mail.com`,
+    };
 };
