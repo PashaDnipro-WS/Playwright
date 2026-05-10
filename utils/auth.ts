81f699da42c2c3dd9e2ead2a8a5be45e262832b0
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-05-10 22:30:25 +0300

    change auth.ts: add auth helper with two factor handling

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,9 @@
-import { type Page } from '@playwright/test';
+import { expect, type Page } from '@playwright/test';
 
 import { LoginPage } from '../pages/LoginPage';
+
 import { users } from '../data/users';
+import { urls } from './urls';
 
 export async function loginAsValidUser(page: Page) {
     const loginPage = new LoginPage(page);
@@ -12,4 +14,20 @@ export async function loginAsValidUser(page: Page) {
         users.validUser.login,
         users.validUser.password
     );
+
+    if (page.url().includes(urls.twoFactorConfirmation)) {
+        await page.pause();
+    }
+
+    await expect(page).toHaveURL(
+        new RegExp(`${urls.myPage}$`)
+    );
+}
+
+export async function expectUserIsLoggedIn(page: Page) {
+    await expect(
+        page.getByRole('link', {
+            name: 'Sign out',
+        })
+    ).toBeVisible();
 }
