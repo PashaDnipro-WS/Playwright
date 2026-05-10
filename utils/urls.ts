6ba59357f75215c393d9d1b8b3c7afac39148e04
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-05-10 13:32:13 +0300

    change name of spec-file and add urls configuration for Redmine routes

---
+++ utils/urls.ts
@@ -0,0 +1,10 @@
+export const urls = {
+  main: '/',
+  login: '/login',
+  register: '/account/register',
+  account: '/my/account',
+  myPage: '/my/page',
+  activity: '/projects/redmine/activity',
+  repository: '/projects/redmine/repository',
+  statistics: '/projects/redmine/repository/statistics',
+};
