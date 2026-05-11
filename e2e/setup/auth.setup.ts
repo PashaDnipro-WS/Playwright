10ccbb904133747cc2302c8af944949b45fce811
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-05-11 13:44:39 +0300

    add auth.setup.ts separately

---
+++ e2e/setup/auth.setup.ts
@@ -0,0 +1,11 @@
+import { test as setup } from '@playwright/test';
+
+import { loginAsValidUser } from '../../utils/auth';
+
+setup('authenticate user', async ({ page }) => {
+    await loginAsValidUser(page);
+
+    await page.context().storageState({
+        path: 'playwright/.auth/user.json',
+    });
+});
