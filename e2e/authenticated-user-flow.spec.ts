246475b706e9b939319f900b20e894ddb9d1c75a
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-05-11 18:23:49 +0300

    set Chrome as an only test browser and append the first auth test

--- e2e/authenticated-user-flow.spec.ts
+++ e2e/authenticated-user-flow.spec.ts
@@ -1,15 +1,23 @@
 import { test, expect } from '../fixtures/fixtures';
+import { urls } from '../utils/urls'
 
 test.use({
     storageState: 'playwright/.auth/user.json',
 });
 
-test('@auth user can open account page with saved session', async ({
+test('@auth user can open account page and logout', async ({
     accountPage,
+    page
 }) => {
     await accountPage.goto();
 
     await accountPage.expectOpened();
 
     await expect(accountPage.header.signOutLink).toBeVisible();
+
+    await accountPage.header.signOut();
+
+    // await expect(page).toHaveURL(urls.main);
+
+    await expect(accountPage.header.signInLink).toBeVisible();
 });
